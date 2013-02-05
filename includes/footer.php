af9cf23d382751b3057f9171a238084fdc6583fa
webdigga <webdigga42@gmail.com>
2013-02-05 18:45:44 +0000

    Social Media hook up

--- includes/footer.php
+++ includes/footer.php
@@ -1,15 +1,15 @@
-        <footer>
-            <div class="main">	
+		<footer>
+			<div class="main">	
 				<ul class="socialMediaIcons">
 					<li>
-						<img src="img/socialMedia/facebook.png"/>					
+						<a href="http://www.facebook.com/pages/AppcidentAutoApp/235581083237070" target="_blank"><img src="img/socialMedia/facebook.png" /></a>			
 					</li>
 					<li>
-						<img src="img/socialMedia/twitter.png"/>	
+						<a href="http://www.twitter.com/AppcidentApp" target="_blank"><img src="img/socialMedia/twitter.png"/></a>
 					</li>
 				</ul>
-                <p>App-cident &copy; <?php echo date("Y"); ?> : <a href="privacy.php">PRIVACY POLICY</a></p>
-            </div>
-        </footer>
+				<p>App-cident &copy; <?php echo date("Y"); ?> : <a href="privacy.php">PRIVACY POLICY</a></p>
+			</div>
+		</footer>
 	</body>
 </html>
